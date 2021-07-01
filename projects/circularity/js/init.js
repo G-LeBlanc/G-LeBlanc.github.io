279b30c8e215f8d55909b64f94812058faba6f5c
G-LeBlanc <cadetgleblanc@nomma.net>
2021-07-01 20:21:55 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,25 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle
+        var circles = []
 
         // TODO 2 : Create a function that draws a circle 
-        
-
+       var  drawCircle = function(){
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle);
+       }
         // TODO 3 / 8 : Call the drawCircle() function 
-
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        for (var loopsCompleted = 0; loopsCompleted < 10; loopsCompleted++) {
+            
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -37,16 +49,24 @@ var init = function (window) {
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-        function update() {
+       var update =  function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            myArray([0])
+            myArray([1])  
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+                                
+            if (circle.x > canvas.width) {
+                circle.x = 0;
+            }
             // TODO 9 : Iterate over the array
            
             
+        }
+        for (var i = 0; i < myArray.length; i++) {
+            var eachValue = myArray[i];
+        
+            physikz.updatePosition(eachCircle)
+            game.checkCirclePosition(eachCircle)
         }
     
         /* 
